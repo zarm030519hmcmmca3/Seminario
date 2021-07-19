056e3e731f60e18294919e3f906be86ac2e77805
zarm030519hmcmmca3 <87658993+zarm030519hmcmmca3@users.noreply.github.com>
2021-07-19 12:11:25 -0500

    Create meta_datos.php

---
+++ archivos_php/meta_datos.php
@@ -0,0 +1,12 @@
+<? php
+$ titulo_sitio = "Registro simple de usuarios" ;
+$ author = "Seminario de Titulación 2021 cecytem tx". ;
+$ viewport = "width = device-width, initial-scale = 1.0" ;
+$ palabras clave = "registro, registro simple, registro simple de usuarios" ;
+$ descripción = "Registro simple de usuarios con PHP y MySQL" ;
+
+$ meta_nombre1 = "autor" ;
+$ meta_nombre2 = "ventana gráfica " ;
+$ meta_nombre3 = "palabras clave" ;
+$ meta_nombre4 = "descripción" ;
+?>
